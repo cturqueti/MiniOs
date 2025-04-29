27a5388d2e40291e97e7a9616efc44c8992e0f62
Carlos Augusto D'Orazio Turqueti <cturqueti@yahoo.com.br>
2025-04-29 15:35:19 -0300

    .

--- src/aes_gcm/gf128mul.h
+++
@@ -1,9 +0,0 @@
-#ifndef GF128MUL_H
-#define GF128MUL_H
-
-#include <stdint.h>
-
-// Multiplicação no campo GF(2^128) usando polinômio x^128 + x^7 + x^2 + x + 1
-void gf128_multiply(const uint8_t *x, const uint8_t *y, uint8_t *result);
-
-#endif // GF128MUL_H
